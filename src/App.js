0a36eac6f53eb9d88faabea6c49d88e26df22de7
Tony Hsu <tdh425@gmail.com>
2021-12-11 17:18:02 -0700

    Add search feature

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
     super()
     this.state = {
       movies: [],
-      error: ''
+      error: '',
+      search:'',
+      filteredMovies: []
     }
   }
 
@@ -24,28 +26,50 @@ class App extends Component {
       })
   }
 
+  handleChange = event => {
+    this.setState({search: event.target.value})
+    const searchedMovies = this.state.movies.filter(movie => {
+      return movie.title.toLowerCase().includes(`${ event.target.value }`)
+    })
+    this.setState({ filteredMovies: searchedMovies })
+    if (!event.target.value) {
+      this.setState({ filteredMovies: [] })
+    }
+  }
+
+  clearSearch = () => {
+    this.setState({search: '', filteredMovies: []})
+  }
+
   render() {
     if (this.state.error) {
       return (
         <h2>Oops, something went wrong. Try again later. Error: '{this.state.error}'</h2>
       )
-    }
-    
+    } 
     return (
       <>
         <header className='header'>
           <Link to='/'>
             <img
               className='title'
-              src="https://fontmeme.com/permalink/211204/cca36d9d02af58d8feae92729d642f28.png"
+              src="https://fontmeme.com/permalink/211204/cca36d9d02af58d8feae92729d642f28.png" 
+              onClick={event => this.clearSearch()}
               alt="Rancid Tomatillos Title"
             />
           </Link>
+          <input
+            type='text'
+            placeholder='Search by Movie Title'
+            name='search'
+            value={this.state.search}
+            onChange={event => this.handleChange(event)}
+          />
         </header>
         <main className='body'>
           <Routes>
             <Route path="/" element={<MovieContainer 
-              movies={this.state.movies}
+              movies={this.state.filteredMovies.length ? this.state.filteredMovies : this.state.movies}
               showMovie={this.showMovie} />}
             />
             <Route path="/:movieId" element={<Wrapper />} />

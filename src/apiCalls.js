f11543ee9b3aab70fd417a2a8e58c3f0f9265e8f
Tony Hsu <tdh425@gmail.com>
2021-12-12 13:59:59 -0700

    Create separate api file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,6 +4,24 @@ let apiCalls = {
         return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
             .then((res) => res.json())
             .catch((err) => err)
+    },
+
+    singleMovie(movieID) {
+        return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieID}`)
+        .then(res => {
+            if (res.ok) {
+              return res.json()
+            } else {
+              throw new Error()
+            }
+        })
+        .catch((err) => err)
+    },
+
+    singleMovieVideo(movieID) {
+        return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieID}/videos`)
+        .then((res) => res.json())
+        .catch((err) => err)
     }
 }
 
